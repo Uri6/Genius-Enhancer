215d2ab5f600c363931d3b7f347ad9d29f0535c6
Reece Dunham <me@rdil.rocks>
2023-02-19 17:49:47 -0500

    ignore lib directory

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,3 +5,9 @@ export function containsHebrew(text) {
 export function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
+
+export function isSongPageUrl(urlPart) {
+    return !urlPart.includes("/") && (urlPart.endsWith("-lyrics") || urlPart.endsWith("-lyrics/") || urlPart.endsWith("-annotated") || urlPart.endsWith("-annotated/") || urlPart.endsWith("?react=1") || urlPart.endsWith("?react=1/") || urlPart.endsWith("?bagon=1") || urlPart.endsWith("?bagon=1/"));
+}
+
+export const geniusAddress = ["http://www.genius.com/", "https://www.genius.com/", "http://genius.com/", "https://genius.com/"];
