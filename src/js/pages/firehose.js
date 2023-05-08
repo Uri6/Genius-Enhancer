6e30b3a90bb2f0b686d2a97260c735dfa5ba9a9f
Uri Sivani <93275900+Uri6@users.noreply.github.com>
2023-05-08 05:14:23 +0300

    FIX: Firehose bugs (caused by cleanup)

--- src/js/pages/firehose.js
+++ src/js/pages/firehose.js
@@ -1,32 +1,49 @@
+async function insertFirehoseCSS(tabId) {
+    await chrome.scripting.insertCSS({
+        target: { tabId },
+        files: ["./src/css/firehose.css"],
+    });
+}
+
+async function executeFirehoseScript(tabId) {
+    function firehoseScript() {
+        function updateCheckboxes() {
+            $("fieldset input[type='checkbox']").each(function () {
+                // If the checkbox is already a custom checkbox, skip it
+                if ($(this).hasClass("chkboxm")) {
+                    return;
+                }
+
+                // Get the attributes of the checkbox
+                let attributes = Array.from(this.attributes)
+                    .map(attr => `${attr.name}="${attr.value}"`)
+                    .join(' ');
+
+                // Replace the checkbox with a custom checkbox
+                let replacement = `<input type="checkbox" name="${this.name}" class="chkboxm" ${attributes}><label for="${this.name}"><span class="chkboxmspan"></span>${$(this).next().html()}</label>`;
+                $(this).replaceWith(replacement);
+            });
+        }
+
+        function removeEmptyLabels() {
+            $("fieldset label").each(function () {
+                if (!$(this).children().length) {
+                    $(this).remove();
+                }
+            });
+        }
+
+        updateCheckboxes();
+        removeEmptyLabels();
+    }
+
+    await chrome.scripting.executeScript({
+        target: { tabId },
+        func: firehoseScript,
+    });
+}
+
 export async function handleFirehose(tabId) {
-    await chrome.scripting.insertCSS(
-        {
-            target: { tabId: tabId },
-            files: ["./src/css/firehose.css"]
-        }
-    );
-
-    await chrome.scripting.executeScript(
-        {
-            target: { tabId: tabId },
-            func: (() => {
-                $("fieldset input[type='checkbox']").each(function() {
-                    if ($(this).hasClass("chkboxm")) {
-                        return;
-                    }
-                    let attributes = "";
-                    for (let i = 0; i < this.attributes.length; i++) {
-                        attributes += this.attributes[i].name + "=\"" + this.attributes[i].value + "\" ";
-                    }
-                    $(this).replaceWith("<input type=\"checkbox\" name=\"" + this.name + "\" class=\"chkboxm\" " + attributes + "><label for=\"" + this.name + "\"><span class=\"chkboxmspan\"></span>" + $(this).next().html() + "</label>");
-                });
-
-                $("fieldset label").each(function() {
-                    if ($(this).children().length) {
-                        $(this).remove();
-                    }
-                });
-            })
-        }
-    );
+    await insertFirehoseCSS(tabId);
+    await executeFirehoseScript(tabId);
 }
