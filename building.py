d922670a695dcb5d3de5e02ade921710ef1d03c6
Uri6 <93275900+Uri6@users.noreply.github.com>
2023-06-01 19:31:27 +0300

    Refactoring the building script

--- building.py
+++ building.py
@@ -3,119 +3,72 @@ import shutil
 import json
 import csscompressor
 
-
-def add_license_comment(directory):
-    license_comment = """/*
- * This code is licensed under the terms of the "LICENSE.md" file
- * located in the root directory of this code package.
- */"""
-    HTML_comment = """<!--
-  -- This code is licensed under the terms of the "LICENSE.md" file
-  -- located in the root directory of this code package.
-  -->"""
-    lib_comment = """/*
- * This code was not written by me (Uri Sivani) and is subject to the license of the original project.
- * Please refer to the original project website for more information.
- */"""
-    for root, dirs, files in os.walk(directory):
-        if "Builds" not in root.split("\\"):
-            for file in files:
-                if file.endswith(".js") or file.endswith(".css") or file.endswith(".scss") or file.endswith(".sass") or file.endswith(".css.map"):
-                    file_path = os.path.join(root, file)
-                    if "lib" in root.split("\\"):
-                        comment = lib_comment
-                    else:
-                        comment = license_comment
-                    with open(file_path, "rb") as f:
-                        lines = f.read().decode("utf-8")
-                    if not lines.startswith(comment) and not lines.startswith('@charset "UTF-8";'):
-                        lines = comment + "\n\n" + lines
-                        with open(file_path, "wb") as f:
-                            f.write(lines.encode("utf-8"))
-                elif file.endswith(".html"):
-                    file_path = os.path.join(root, file)
-                    with open(file_path, "rb") as f:
-                        lines = f.read().decode("utf-8")
-                    if not lines.startswith(HTML_comment):
-                        lines = HTML_comment + "\n\n" + lines
-                        with open(file_path, "wb") as f:
-                            f.write(lines.encode("utf-8"))
-
-
 def copytree(src, dst):
-    if os.name == 'nt':  # for Windows
-        src = src.replace('/', '\\')
-        dst = dst.replace('/', '\\')
-        sep = '\\'
-    else:  # for Mac
-        sep = '/'
-    print('Copying {} to {}'.format(src, dst))
+    sep = os.sep
+    print(f'Copying {src} to {dst}')
     for folder in os.listdir(src):
-        skip = False
-        bad_folders = [".vscode", ".git", ".github",
-                       "Builds", "vision", "header",
-                       "gifs", "screenshots", ".idea",
-                       ".yarn", ".venv"]
-        if folder in bad_folders or src.endswith(sep + "icons") and (folder == "1" or folder == "2") or (src.endswith(sep + "bio") or src.endswith(sep + "people") or src.endswith(sep + "releaseDate") or src.endswith(sep + "magicWand")) and folder == "1":
-            continue
-        if os.path.isdir(src + sep + folder):
-            os.mkdir(dst + sep + folder)
-            copytree(src + sep + folder, dst + sep + folder)
-        else:
-            file = folder
-            bad_files = [".py", ".ts", ".map", ".scss", ".sass",
-                         ".psd", ".gitignore", "todo", "changelog.md",
-                         "tsconfig.json", "package.json", "yarn.lock",
-                         ".yarnrc.yml", ".prettierignore", ".env.example",
-                         ".env"]
-            for bad_file in bad_files:
-                if file.endswith(bad_file):
-                    skip = True
-            nedded_dimensions = {"artwork": "512x512.png", "Exists": "64x64.png", "Missing": "64x64.png", "icons"+sep+"3": [
-                "16x16.png", "32x32.png", "48x48.png", "128x128.png"]}
-            for key in nedded_dimensions:
-                if src.endswith(sep + key):
-                    if type(nedded_dimensions[key]) == list:
-                        if file not in nedded_dimensions[key]:
-                            skip = True
-                    else:
-                        if file != nedded_dimensions[key]:
-                            skip = True
-            if not skip:
-                shutil.copy(src + sep + file, dst + sep + file)
-                # Compress CSS files
-                if file.endswith('.css'):
-                    with open(dst + sep + file, 'r', encoding='utf-8') as f:
-                        print('Compresing {}'.format(dst + sep + file))
-                        data = csscompressor.compress(f.read())
-                    with open(dst + sep + file, 'w', encoding='utf-8') as f:
-                        f.write(data)
+        if not should_skip_folder(src, folder):
+            if os.path.isdir(src + sep + folder):
+                os.mkdir(dst + sep + folder)
+                copytree(src + sep + folder, dst + sep + folder)
+            else:
+                file = folder
+                if not should_skip_file(src + sep + file):
+                    shutil.copy(src + sep + file, dst + sep + file)
+                    if file.endswith('.css'):
+                        compress_css_file(dst + sep + file)
 
+def should_skip_folder(src, folder):
+    if not os.path.isdir(src + os.sep + folder):
+        return False
+    bad_folders = [".vscode", ".git", ".github", "Builds", "vision", "header", "gifs", "screenshots", ".idea", ".yarn", ".venv", "arrows", "settings"]
+    if folder in bad_folders:
+        return True
+    if src.endswith(os.sep + "icons") and folder in ["1", "2"]:
+        return True
+    if src.endswith(os.sep + "bio") or src.endswith(os.sep + "people") or src.endswith(os.sep + "releaseDate") or src.endswith(os.sep + "magicWand"):
+        if folder == "1":
+            return True
+    return False
 
-rootDir = os.path.dirname(os.path.realpath(__file__))
+def should_skip_file(file):
+    bad_files = [".py", ".ts", ".scss", ".psd", ".gitignore", ".gitattributes", "todo", "changelog.md", "compileAllSass.js", "postcssCompile.js", "tsconfig.json", "package.json", "yarn.lock", ".yarnrc.yml", ".prettierignore", ".env.example", ".env"]
+    if any(file.endswith(bad_file) for bad_file in bad_files):
+        return True
+    nedded_dimensions = {"artwork": "512x512.png", "Exists": "64x64.png", "Missing": "64x64.png", "Simple": "32x32.png", "magicWand"+os.sep+"2": "32x32.png", "icons"+os.sep+"3": ("16x16.png", "32x32.png", "48x48.png", "128x128.png")}
+    print(os.path.dirname(file))
+    for folder, dimensions in nedded_dimensions.items():
+        if folder in os.path.dirname(file):
+            if isinstance(dimensions, str):
+                if not file.endswith(dimensions):
+                    return True
+            elif isinstance(dimensions, tuple):
+                if not any(file.endswith(dimension) for dimension in dimensions):
+                    return True
+    return False
 
+def compress_css_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        print(f'Compressing {file_path}')
+        data = csscompressor.compress(f.read())
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(data)
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
 manifest = open('./manifest.json', 'r')
-
 data = json.load(manifest)
 version = data.get('version')
-
 manifest.close()
-
-dst_path = rootDir + '/Builds/' + version
-
-# add_license_comment(rootDir)
-
+dst_path = os.path.join(root_dir, 'Builds', version)
 if os.path.exists(dst_path):
     shutil.rmtree(dst_path)
-
 os.mkdir(dst_path)
-
-copytree(rootDir, dst_path)
-
-# remove any .zip files in the Builds folder
-for file in os.listdir(rootDir + '/Builds'):
+copytree(root_dir, dst_path)
+for file in os.listdir(os.path.join(root_dir, 'Builds')):
     if file.endswith('.zip'):
-        os.remove(rootDir + '/Builds/' + file)
-
-# zip the folder
+        os.remove(os.path.join(root_dir, 'Builds', file))
+# delete all the empty folder in the new version folder
+for folder in os.listdir(dst_path):
+    if os.path.isdir(os.path.join(dst_path, folder)) and not os.listdir(os.path.join(dst_path, folder)):
+        os.rmdir(os.path.join(dst_path, folder))
 shutil.make_archive(dst_path, 'zip', dst_path)
